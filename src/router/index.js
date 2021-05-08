50730823402d020e6cb6df167eb97b409c9f0a54
luke951 <info.lukaszkaliniak@gmail.com>
2021-05-08 12:42:13 +0200

    repo init

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'About',
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	}
+]
+
+const router = new VueRouter({
+	mode: 'history',
+	base: process.env.BASE_URL,
+	scrollBehavior (to, from, savedPosition) {
+		return { x: 0, y: 0 }
+	},
+	routes
+})
+
+export default router
